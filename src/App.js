7effb0113057547cf731676f5307959498a6a541
ManaswiniSasmal <manisasmal5597@gmail.com>
2023-10-08 16:37:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Restaurant from './component/Basics/Restaurant';
+import UseState from './Hooks/useState';
+// import UseEffect from './component/Hooks/useEffect';
+// import UseReducer from './component/Hooks/useReducer';
+//import ToDo from './component/ToDoList/ToDo';
+// import Temp from './component/Weather/Temp';
+const App = () => {
+   return <UseState />;
+  
+};
+
+export default App;
